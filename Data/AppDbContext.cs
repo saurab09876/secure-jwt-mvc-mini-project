721fe96a4eb26c8088a9fef008533ab91d9e02a4
saurab09876 <sauravsharma09876@gmail.com>
2026-02-01 19:15:54 +0530

    Add EF Core DbContext, User and RefreshToken models with migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureJwtMiniProject.Models;
+
+namespace SecureJwtMiniProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
+
---
+++ Models/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿namespace SecureJwtMiniProject.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
